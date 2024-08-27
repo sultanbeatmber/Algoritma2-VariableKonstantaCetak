c526754612bf4325a68161c4b4bd8bfd494e059e
abangmuslim <abangmuslimtamiang@gmail.com>
2024-08-28 02:06:03 +0700

    merubah no fie & update mencetak dengan echo

--- 9 mencetak dengan echo.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// membuat variabel baru
-$nama_barang = "Minyak Goreng";
-$harga = 15000;
-$tempat = "kuala simpang";
-
-// menampilkan isi variabel
-echo "Ahmadi muslim membeli $nama_barang seharga Rp $harga di $tempat";
